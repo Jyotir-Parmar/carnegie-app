d2eb4d959b3ed8a62352238d9838bd0c61088ac7
Jyotir Parmar <jyotiraditya.parmar@gmail.com>
2020-05-30 21:34:13 +0530

    app

---
+++ precache-manifest.b9ad1c187265c26a629f3348973fdd2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40f66a59caf88ad29007c7ab4c476801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1ea57662eaa9f2b6be0",
+    "url": "/static/css/main.804cb6bd.chunk.css"
+  },
+  {
+    "revision": "2a4bd7d928f7a3c0cea1",
+    "url": "/static/js/2.c7027e69.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c7027e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ea57662eaa9f2b6be0",
+    "url": "/static/js/main.fac0ad65.chunk.js"
+  },
+  {
+    "revision": "96f640751e2d5a3c3b1e",
+    "url": "/static/js/runtime-main.bb28ea36.js"
+  },
+  {
+    "revision": "a588182c71bfd540592cf05021af390f",
+    "url": "/static/media/banner.a588182c.png"
+  }
+]);
